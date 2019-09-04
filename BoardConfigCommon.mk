0066d640915658015c57313d2a651736bfcc5c0e
Volodymyr Zhdanov <wight554@gmail.com>
2019-09-04 17:44:21 +0300

    [DNM]: msm8998-common: permisive for now

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -53,10 +53,10 @@ BOARD_KERNEL_CMDLINE += user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3
 BOARD_KERNEL_CMDLINE += service_locator.enable=1
 BOARD_KERNEL_CMDLINE += swiotlb=2048
 BOARD_KERNEL_CMDLINE += loop.max_part=7
+BOARD_KERNEL_CMDLINE += firmware_class.path=/vendor/firmware_mnt/image
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 
-TARGET_COMPILE_WITH_MSM_KERNEL := true
-
-BUILD_BROKEN_DUP_RULES := true
+TARGET_COMPILE_WITH_MSM_KERNEL := true                                                                          BUILD_BROKEN_DUP_RULES := true
 
 # Platform
 TARGET_BOARD_PLATFORM := msm8998
@@ -205,6 +205,7 @@ include device/qcom/sepolicy-legacy-um/sepolicy.mk
 BOARD_SEPOLICY_DIRS += $(PLATFORM_PATH)/sepolicy/vendor
 BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(PLATFORM_PATH)/sepolicy/public
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(PLATFORM_PATH)/sepolicy/private
+SELINUX_IGNORE_NEVERALLOWS := true
 
 # Treble
 BOARD_PROPERTY_OVERRIDES_SPLIT_ENABLED := true
